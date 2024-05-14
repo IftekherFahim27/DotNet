426374fffe37b4c15e32a24d7ee7af7cae5ebfeb
IftekherFahim27 <fahimthescientist@gmail.com>
2024-05-14 11:35:19 +0600

    Add project files.

---
+++ DotNet/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNet.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = "";
+    }
+}
---
+++ DotNet/Services/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DotNet.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet.Services
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>()
+                .HasIndex(c => c.CategoryName)
+                .IsUnique();
+        }
+    }
+}
